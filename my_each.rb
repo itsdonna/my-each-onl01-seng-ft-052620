875944d5f8dab326d68942a7f6a5e4398a900c42
Donna Torres <todonnatorres@icloud.com>
2020-06-01 16:19:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,6 +2,12 @@ def my_each # put argument(s) here
   # code here
   
 end
+
+
 #each loops through an Array
 #does something here with the elements of an Array
 #returns the original array
+
+# my_each(collection) do |i|
+#   puts i
+# end
