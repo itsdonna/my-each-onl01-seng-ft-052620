d06799a0b72314a9acbb33583500a649f0951347
Donna Torres <todonnatorres@icloud.com>
2020-06-01 16:39:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array)# put argument(s) here
   counter = 0
   while counter < array.length # [blue, red, green, yellow]
-    # yield array[counter]
+    yield array[counter]
     counter += 1
   end
   
